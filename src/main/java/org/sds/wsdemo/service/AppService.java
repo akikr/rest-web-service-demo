12477ef75ea9c37aaf446dc25396dc9704d3b814
ankit <ankit.kumar@seamless.se>
2021-06-22 22:26:46 +0530

    Modified: remove the Client & chnaged the AppService funtion signature

--- src/main/java/org/sds/wsdemo/service/AppService.java
+++ src/main/java/org/sds/wsdemo/service/AppService.java
@@ -1,10 +1,20 @@
 package org.sds.wsdemo.service;
 
 import org.sds.wsdemo.modal.User;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/api")
 public interface AppService {
 
+    @GetMapping(value = "/user",
+            consumes = MediaType.TEXT_PLAIN_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     User getUser(String username);
 
+    @GetMapping(value = "/userV2",
+            consumes = MediaType.TEXT_PLAIN_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     User getUserV2(String username);
 }
